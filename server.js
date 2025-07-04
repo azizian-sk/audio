582e5f6a3b9cd8514f241d8bc4a5a83de4ef67c9
azizian-sk <33261314+azizian-sk@users.noreply.github.com>
2025-07-04 20:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log("Server listening on port", PORT);
 });
